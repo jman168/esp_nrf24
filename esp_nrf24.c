3c3c0f9b883d5d029f1b76adad29ff61f3f00797
Jason Beckmann <jasonabeckmann@gmail.com>
2020-11-26 02:54:33 -0500

    Ddded handle structure and device initialization and deactivation

--- esp_nrf24.c
+++ esp_nrf24.c
@@ -1,7 +1,57 @@
 #include <stdio.h>
 #include "esp_nrf24.h"
 
-void func(void)
-{
+esp_err_t init_nrf24(nrf24_t *dev, spi_host_device_t host_id, int mosi_io_num, int miso_io_num, int sclk_io_num, int ce_io_num, int csn_io_num) {
+    esp_err_t ret;
+    
+    gpio_pad_select_gpio( ce_io_num );
+	gpio_set_direction( ce_io_num, GPIO_MODE_OUTPUT );
+	gpio_set_level( ce_io_num, 0 );
 
+	gpio_pad_select_gpio( csn_io_num );
+	gpio_set_direction( csn_io_num, GPIO_MODE_OUTPUT );
+	gpio_set_level( csn_io_num, 1 );
+
+    const spi_bus_config_t config = {
+        mosi_io_num: mosi_io_num,
+        miso_io_num: miso_io_num,
+        sclk_io_num: sclk_io_num,
+        quadwp_io_num: -1,
+        quadhd_io_num: -1,
+    };
+    ret = spi_bus_initialize(host_id, &config, 1);
+    ESP_LOGI(TAG, "Initalized SPI bus, status: %d", ret);
+    if(ret != ESP_OK) return ret;
+    
+    const spi_device_interface_config_t devcfg = {
+		.clock_speed_hz = SPI_FREQUENCY,
+		.queue_size = 7,
+		.mode = 0,
+		.flags = SPI_DEVICE_NO_DUMMY,
+	};
+    spi_device_handle_t handle;
+    ret = spi_bus_add_device(host_id, &devcfg, &handle);
+    ESP_LOGI(TAG, "Added SPI bus device, status: %d", ret);
+    if(ret != ESP_OK) return ret;
+
+    dev->host_id = host_id;
+    dev->spi_handle = handle;
+    dev->ce_io_num = ce_io_num;
+    dev->csn_io_num = csn_io_num;
+
+    return ESP_OK;
+}
+
+esp_err_t free_nrf24(nrf24_t *dev) {
+    esp_err_t ret;
+
+    ret = spi_bus_remove_device(dev->spi_handle);
+    ESP_LOGI(TAG, "Removed SPI device, status: %d", ret);
+    if(ret != ESP_OK) return ret;
+
+    ret = spi_bus_free(dev->host_id);
+    ESP_LOGI(TAG, "Freed SPI bus, status: %d", ret);
+    if(ret != ESP_OK) return ret;
+
+    return ESP_OK;
 }
--- include/esp_nrf24.h
+++ include/esp_nrf24.h
@@ -1,1 +1,18 @@
-void func(void);
+#include "hal/spi_types.h"
+#include "driver/spi_common.h"
+#include "driver/spi_master.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+
+#define SPI_FREQUENCY 40000000
+#define TAG "NRF24"
+
+typedef struct {
+    spi_host_device_t host_id;
+    spi_device_handle_t spi_handle;
+    int ce_io_num;
+    int csn_io_num;
+} nrf24_t;
+
+esp_err_t init_nrf24(nrf24_t *dev, spi_host_device_t host_id, int mosi_io_num, int miso_io_num, int sclk_io_num, int ce_io_num, int csn_io_num);
+esp_err_t free_nrf24(nrf24_t *dev);
